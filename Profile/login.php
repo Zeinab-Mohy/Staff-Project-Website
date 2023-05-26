0f250c020fa2e9fd082208d67920a530bddde360
Zeinab Mohy <95243799+Zeinab-Mohy@users.noreply.github.com>
2023-05-26 12:51:50 +0300

    Add files via upload

---
+++ Profile/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+	<link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+<div class="container">
+    <div class="title">LogIn</div>
+    <form method="post" action="./submit_log_in.php">
+        <div class="user-details">
+            <div class="input-box">
+                <span class="details">Email</span>
+                <input type="text" name="email" placeholder="Enter Your email" required>
+            </div>
+            <div class="input-box">
+                <span class="details">Password</span>
+                <input type="password" name="password" placeholder="Enter Your password" required>
+            </div>
+        </div>
+              
+        <div class="button">
+            <input type="submit" name="sign_in" value = "Sign in ">
+        </div>
+
+        <div class="text sign-up-text">Don't have an account? <a
+                        href="signup.php">SIGN UP</a></label></div>
+    </form>
+
+
+</div>
+</body>
+</html>
---
+++ Profile/profile_search.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Profile</title>
+	<link rel="stylesheet" type="text/css" href="css/profile.css">
+    <style>
+        table,thead,th,td{
+            border: 1px solid;
+            width: 2000px;
+            height: 50px;
+            text-align: center;
+        } 
+    </style>
+</head>
+<body>
+    <?php
+        $a=$_POST['first_name'];
+        $b=$_POST['second_name'];
+        $c=$_POST['email'];
+
+        $conn = new PDO('mysql:host=localhost;dbname=ia_project','root','');
+        $query="SELECT *FROM staff where first_name='$a' and
+        second_name='$b' and email='$c'";
+        $sql=$conn->prepare($query);
+        $sql->execute();
+        $users=$sql->fetchALL();
+        // var_dump($users);
+    ?>
+	<div class="container">
+		
+			
+        <div class="profile-card"> 
+
+        <h1>My Profile</h1>
+		<form action="" method="post" name="display">
+        <?php foreach($users as $user):?>
+            <div >
+            <img src="profile_img/<?php $image=$user["image"];echo $user["image"]; ?>" width = 200 title="<?php echo $user['image']; ?>" class="image">
+            </div>
+
+    <!-- <div class="test-data"> -->
+    <!-- <form class="" action="#" method="post" autocomplete="off" enctype="multipart/form-data"> -->
+        <div class="user-details">
+            <div class="input-box">
+                <label class="profile-details"> First Name </label>
+                <p><?php $first_name=$user['first_name'] ;echo $user['first_name']." ";?></p>
+                
+            </div>
+
+            <div class="input-box">
+                <label class="profile-details">Second Name </label>
+                <p><?php $second_name=$user['second_name'] ;echo $user['second_name']." ";?></p>
+            </div>
+
+            <div class="input-box">
+                <label class="profile-details">Email</label>
+                <p><?php $email=$user['email'];echo $user['email'];?></p>
+            </div>
+
+            <div class="input-box">
+                <label class="profile-details">Faculty</label>
+                <p><?php $faculty=$user['faculty'];echo $user['faculty'];?></p>
+            </div>
+
+            <div class="input-box">
+                <label class="profile-details">Scientific Degree</label>
+                <p><?php $scientific_degree=$user['scientific_degree'];echo $user['scientific_degree'];?></p>
+            </div>
+
+            <div class="input-box">
+                <label class="profile-details">Gender</label>
+                <p><?php $gender=$user['gender'];
+                            $password=$user["password"] ;
+                            $id=$user["id"];
+                            echo $user['gender'];?></p>
+            </div>
+
+
+            <div class="input-box">
+                <label class="profile-details">Nationality </label>
+                <p><?php $nationality=$user['nationality'];echo $user['nationality'];?></p>
+            </div>
+            <?php endforeach;?>
+        </form>
+    </div>
+</div>
+</div>
+</body>
+</html>
